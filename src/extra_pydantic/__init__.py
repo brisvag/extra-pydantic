c730dda27613319146b75c955fe996513b8e735c
Lorenzo Gaifas <brisvag@gmail.com>
2022-09-10 13:34:22 +0200

    change authors/site information

--- src/extra_pydantic/__init__.py
+++ src/extra_pydantic/__init__.py
@@ -6,8 +6,6 @@ try:
     __version__ = version("extra-pydantic")
 except PackageNotFoundError:
     __version__ = "uninstalled"
-__author__ = "Talley Lambert"
-__email__ = "talley.lambert@gmail.com"
 
 from .main import BaseModel, ModelMetaclass, create_model
 from .monkeypatch import patched_pydantic_base_model
